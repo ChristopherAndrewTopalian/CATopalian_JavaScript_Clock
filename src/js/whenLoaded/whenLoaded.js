67c7997e53602881a0a08dc912365570c591764f
Christopher Andrew Topalian <scriptingdata@gmail.com>
2026-01-14 07:48:17 -0500

    update

--- src/js/whenLoaded/whenLoaded.js
+++ src/js/whenLoaded/whenLoaded.js
@@ -13,12 +13,19 @@ function whenLoaded()
     {
         whenWindowResized();
     };
+
+    //-//
+
+    if (ge('clock'))
+    {
+        ge('clock').innerHTML = new Date().toLocaleTimeString('en-US');
+    }
 }
 
 //----//
 
 // Dedicated to God the Father
-// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2025
+// All Rights Reserved Christopher Andrew Topalian Copyright 2000-2026
 // https://github.com/ChristopherTopalian
 // https://github.com/ChristopherAndrewTopalian
 // https://sites.google.com/view/CollegeOfScripting
